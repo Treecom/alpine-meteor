ba00647acf24f3685dda6e83ff398eed431d2914
Martin Bučko <bucko@treecom.net>
2016-09-02 18:26:35 +0200

    Udated repository link

--- scripts/install_graphicsmagick.sh
+++ scripts/install_graphicsmagick.sh
@@ -1,5 +1,5 @@
 set -e
 
-apk --update add graphicsmagick --repository http://dl-3.alpinelinux.org/alpine/edge/testing/ --allow-untrusted
+apk --update add graphicsmagick --repository http://dl-3.alpinelinux.org/alpine/edge/community/
 
 sh $METEORD_DIR/clean-soft.sh
