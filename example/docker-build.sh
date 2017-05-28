8f38ea949bf087263c89bcddaee5aae783d4a80e
Chip Parker <infowolfe@gmail.com>
2017-05-28 04:13:06 -0400

    apparently you can't change the build dir out of the current build context w/ docker, so that's not _really_ an option

--- example/docker-build.sh
+++ example/docker-build.sh
@@ -5,7 +5,7 @@ set -e
 CONTAINER=martinez/alpine-meteor-example	# <-- Name your container
 TAG=latest									# <-- Tag for your container
 REGISTRY=false								# <-- If you use other then github repo
-BUILD_DIR=${PWD}/../alpine-build			# <-- This is where meteor build your files.
+BUILD_DIR=${PWD}/.build			# <-- This is where meteor build your files.
 											#     Folder will be created and after build will be deleted
 
 echo "Start building container ${CONTAINER} ..."
